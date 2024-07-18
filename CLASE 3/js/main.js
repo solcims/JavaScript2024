837ac86e17c16ea0abea1554c72b1438bf257617
Sol Martinez Schippert <solschippert@gmail.com>
2024-07-18 17:56:45 -0300

    CLASE 3

---
+++ CLASE 3/js/main.js
@@ -0,0 +1,27 @@
+//? for
+
+
+//? while
+
+// console.log ("Inicio");
+
+// let x = 0; //inicializador
+// while (x < 5) { //condicion que debe cumplirse para que el ciclo se repita
+//     console.log (x); // lo que queremos ejecutar
+//     x = x + 1; //incrementador
+// }
+
+// console.log ("Final");
+
+// mientras x es menos a 5, se va a ejecutar el console log. 
+// Ejecuta cuando la condicion es verdadera.
+//si yo no pongo el incrementador, x quedarpia siempre menor a 0, entonces
+// el while se repite infinitamente. por eso se busca el false (limite)
+
+//? do.while
+
+let y=0;
+do {
+    console.log(y);
+    y = y + 1;
+} while (y<5);
