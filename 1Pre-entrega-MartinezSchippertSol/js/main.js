879d66dbcb5bc25f49e014921367860bb85fc6a8
Sol Martinez Schippert <solschippert@gmail.com>
2024-07-18 18:11:33 -0300

    1Pre-entrega-MartinezSchippertSol

---
+++ 1Pre-entrega-MartinezSchippertSol/js/main.js
@@ -0,0 +1,20 @@
+console.log ("Sol Martinez Schippert - Calcular nota final de alumnos ingresados")
+
+let ingreso = prompt("¿Querés ingresar notas de alumnos o no? Escribí esc para salir");
+let acumulador = 0;
+
+function sumadorDeNotas (){
+    while (ingreso !== "esc") {
+
+        const nota = parseInt(prompt("Ingresá la nota del alumno:"));
+
+        acumulador += nota;
+
+        ingreso = prompt("¿Necesitás ingresar más notas de alumnos o no(esc)? Escribí esc para salir");}
+}
+
+sumadorDeNotas ();
+
+alert("La sumatoria de la nota de los alumnos es de " + acumulador);
+
+alert ("Nos vemos pronto!")
