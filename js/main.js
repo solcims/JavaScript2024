d099c25a249f457089652dc71ed950bfa89a2bc2
Sol Martinez Schippert <solschippert@gmail.com>
2024-08-06 20:55:54 -0300

    2daPreEntrega-SolMartinezSchippert

--- js/main.js
+++ js/main.js
@@ -1,20 +1,45 @@
-console.log ("Sol Martinez Schippert - Calcular nota final de alumnos ingresados")
+console.log("Sol Martinez Schippert - Calcular nota final de alumnos ingresados");
 
-let ingreso = prompt("¿Querés ingresar notas de alumnos o no? Escribí esc para salir");
-let acumulador = 0;
-
-function sumadorDeNotas (){
-    while (ingreso !== "esc") {
-
-        const nota = parseInt(prompt("Ingresá la nota del alumno:"));
-
-        acumulador += nota;
-
-        ingreso = prompt("¿Necesitás ingresar más notas de alumnos o no(esc)? Escribí esc para salir");}
+class Alumno {
+    constructor(nombre, apellido, edad, nota) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.nota = nota;
+    }
 }
 
-sumadorDeNotas ();
+const claseUno = [];
+let acumulador = 0;
 
-alert("La sumatoria de la nota de los alumnos es de " + acumulador);
+function agregarAlumno() {
+    let nombre = prompt("Ingresá el nombre del alumno:");
+    let apellido = prompt("Ingresá el apellido del alumno:");
+    let edad = parseInt(prompt("Ingresá la edad del alumno:"));
+    let nota = parseFloat(prompt("Ingresá la nota del alumno:"));
+    
+    let nuevoAlumno = new Alumno(nombre, apellido, edad, nota);
+    
+    claseUno.push(nuevoAlumno);
+}
 
-alert ("¡Nos vemos pronto!")
+function sumadorDeNotas() {
+    let ingreso = prompt("¿Querés ingresar notas de alumnos o no? Escribí 'no' para salir");
+    
+    while (ingreso !== "no") {
+        agregarAlumno();
+        acumulador += claseUno[claseUno.length - 1].nota;
+        ingreso = prompt("¿Necesitás ingresar más alumnos? Escribí 'no' para salir");
+    }
+}
+
+sumadorDeNotas();
+alert("La sumatoria de las notas de los alumnos es de " + acumulador);
+
+let detallesAlumnos = "Detalles de los alumnos:\n";
+for (let alumno of claseUno) {
+    detallesAlumnos += `Nombre: ${alumno.nombre}, Apellido: ${alumno.apellido}, Edad: ${alumno.edad}, Nota: ${alumno.nota}\n`;
+}
+alert(detallesAlumnos);
+
+alert("¡Nos vemos pronto!");
